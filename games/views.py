8d3baa09fbdc94a0aae0cde42cd5bac62858d2c7
kirilikl <kirill.ahremenko@gmail.com>
2026-03-07 14:50:09 +0300

    1.3.3

--- games/views.py
+++ games/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+GAMES = [
+    "SugarCrash",
+    "Змейка",
+    "Сапёр",
+    "Смута",
+    "World of Tanks",
+    "Flight Simulator",
+    "WarCraft",
+    "Gardenscapes"
+]
 
 # Create your views here.
 def game_list(request):
-    return render(request, "games/game_list.html")
+    query = request.GET.get("q")
+    if query:
+        filtered_games = [game for game in GAMES if query.lower() in game.lower()]
+    else:
+        filtered_games = GAMES
+
+    context = {'games': filtered_games}
+    return render(request, "games/game_list.html", context)
