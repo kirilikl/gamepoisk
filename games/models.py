722fe8895f54e40c564ec91472b1813516c86c1b
kirilikl <kirill.ahremenko@gmail.com>
2026-03-07 15:31:45 +0300

    1.4.1 Начало создания модели

--- games/models.py
+++ games/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+class Game(models.Model):
+    title = models.CharField(max_length=255,
+                             verbose_name="Название игры")
+    image = models.ImageField(upload_to='game_images',
+                              default='default.jpg',
+                              verbose_name="Изображение игры")
+    slug = models.SlugField(max_length=200,
+                            unique=True,
+                            blank=True,
+                            verbose_name="URL-метка")
