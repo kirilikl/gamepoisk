8123c7b91a01a1e79fbb819a6b9ba2fc419b1c71
kirilikl <kirill.ahremenko@gmail.com>
2026-03-14 14:16:32 +0300

    1.4.2

--- games/models.py
+++ games/models.py
@@ -12,3 +12,15 @@ class Game(models.Model):
                             unique=True,
                             blank=True,
                             verbose_name="URL-метка")
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Game, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Игра'
+        verbose_name_plural = 'Игры'
