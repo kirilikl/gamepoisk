7bbaf06936af9961dc4cf57f4f1e5f1f7c5f5107
kirilikl <kirill.ahremenko@gmail.com>
2026-03-28 14:18:34 +0300

    models

--- games/models.py
+++ games/models.py
@@ -8,7 +8,7 @@ class Game(models.Model):
     image = models.ImageField(upload_to='game_images', 
                               default='default.jpg', 
                               verbose_name="Изображение игры")
-    
+
     description = models.TextField(verbose_name="Краткое описание игры")
     release_date = models.DateField(null=True, blank=True, verbose_name="Дата выхода")
     genre = models.CharField(max_length=100, verbose_name="Жанр")
@@ -37,3 +37,14 @@ class Game(models.Model):
     class Meta:
         verbose_name = 'Игра'
         verbose_name_plural = 'Игры'  
+        
+class Screenshot(models.Model):
+    image = models.ImageField(upload_to='screenshots')
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Screenshot for {self.image}'
+
+    class Meta:
+        verbose_name = 'Скриншот'
+        verbose_name_plural = 'Скриншоты'    
