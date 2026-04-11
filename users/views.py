bd7f58dee7163581e00882ca6a6418220a49a4da
kirilikl <kirill.ahremenko@gmail.com>
2026-04-11 15:06:03 +0300

    view

--- users/views.py
+++ users/views.py
@@ -16,3 +16,26 @@ def register(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Привет, {username}!')
+                return redirect('game_list') 
+            else:
+                messages.error(request, 'Неверные имя пользователя или пароль')
+    else:
+        form = CustomAuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    messages.info(request, 'До свидания.')
+    return redirect('game_list') 
