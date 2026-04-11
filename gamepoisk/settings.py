f67419619a29f62ea42d799a389c10d6e91f5d44
kirilikl <kirill.ahremenko@gmail.com>
2026-04-11 15:18:51 +0300

    setttings

--- gamepoisk/settings.py
+++ gamepoisk/settings.py
@@ -126,3 +126,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "users.CustomUser"
+
+LOGIN_REDIRECT_URL = "game_list"
+LOGOUT_REDIRECT_URL = "game_list"
