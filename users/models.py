01e2c1451871ffdf768af48b46442dc4da575e6f
kirilikl <kirill.ahremenko@gmail.com>
2026-04-11 14:28:08 +0300

    new awesome damn good register form

--- users/models.py
+++ users/models.py
@@ -12,9 +12,8 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
-    
     class Meta:
         verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'   
+        verbose_name_plural = 'Пользователи'
 
 
