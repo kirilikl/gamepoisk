4c5c5f0757648f774e0832d542a9fcc6f0c77cda
kirilikl <kirill.ahremenko@gmail.com>
2026-04-18 15:32:24 +0300

    asd

--- games/views.py
+++ games/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Game, Screenshot
 
 # Create your views here.
@@ -10,9 +10,28 @@ def game_list(request):
 
 def game_detail(request, game_id):
     game = get_object_or_404(Game, pk=game_id)
-    screenshot = Screenshot.objects.filter(game=game)
+
+    if request.method == 'POST':
+        comment_text = request.POST.get('comment')
+        if comment_text:
+            comment = Comment.objects.create(
+                game=game,
+                author=request.user,
+                content=comment_text,
+            )
+            request.user.comment_count += 1
+            request.user.save()
+            return redirect('game_detail', game_id=game.id)
+
+    if request.method == 'POST' and 'add_to_library' in request.POST:
+            return redirect('add_to_library', game_id=game.id)
+
+    comments = game.comment_set.all()
+    screenshots = game.screenshots.all()
+
     context = {
-        "game": game,
-        "screenshots": screenshot
+        'game': game,
+        'comments': comments,
+        'screenshots': screenshots,
     }
-    return render(request, "games/game_detail.html", context)
+    return render(request, 'games/game_detail.html', context)
